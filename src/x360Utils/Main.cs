275a3ffdc6a0535256a4cdec77d0a2bb6ec12267
Swizzy <swizzy@hotmail.co.uk>
2013-10-22 21:53:04 +0200

    Added Main.cs containing a function to grab the DLL Version, and added stuff to update versions automatically from now on
    Signed-off-by: Swizzy <swizzy@hotmail.co.uk>

---
+++ src/x360Utils/Main.cs
@@ -0,0 +1,21 @@
+﻿namespace x360Utils
+{
+    using System;
+    using System.Reflection;
+
+    public static class Main {
+        private static readonly Version AppVersion = Assembly.GetAssembly(typeof(Main)).GetName().Version;
+
+        public static string Version {
+            get { 
+#if DEBUG
+                return string.Format("x360Utils v{0}.{1} (Build: {2}) [ DEBUG ]", AppVersion.Major, AppVersion.Minor, AppVersion.Build);
+#elif PRINTDEBUG
+                return string.Format("x360Utils v{0}.{1} (Build: {2}) [ Print Debug Messages ]", AppVersion.Major, AppVersion.Minor, AppVersion.Build);
+#else
+                    return string.Format("x360Utils v{0}.{1} (Build: {2})", AppVersion.Major, AppVersion.Minor, AppVersion.Build);
+#endif
+            }
+        }
+    }
+}
