ae5c1d987791708d95958b006ded61a4d9178713
Swizzy <swizzy@hotmail.co.uk>
2014-02-23 09:59:03 +0100

    Fixed the FUSE class and replaced Retail with FatRetail while adding SlimRetail, Devkit and Testkit fuse types
    Signed-off-by: Swizzy <swizzy@hotmail.co.uk>

--- src/x360Utils/CPUKey/FUSE.cs
+++ src/x360Utils/CPUKey/FUSE.cs
@@ -4,7 +4,7 @@
     using System.Globalization;
     using System.IO;
 
-    internal sealed class FUSE {
+    public sealed class FUSE {
         public readonly UInt64[] FUSELines = new UInt64[12];
 
         public FUSE(string fuseFile) : this(File.ReadAllLines(fuseFile)) { }
@@ -27,67 +27,47 @@
 
         public string CPUKey {
             get {
-                var i = 0;
-                while(true) {
-                    string cpukey;
-                    switch(i) {
-                        case 0:
-                            cpukey = (FUSELines[3] | FUSELines[4]).ToString("X16") + (FUSELines[5] | FUSELines[6]).ToString("X16");
-                            break;
-                        case 1:
-                            cpukey = FUSELines[3].ToString("X16") + FUSELines[5].ToString("X16");
-                            break;
-                        case 2:
-                            cpukey = FUSELines[3].ToString("X16") + FUSELines[6].ToString("X16");
-                            break;
-                        case 3:
-                            cpukey = FUSELines[4].ToString("X16") + FUSELines[5].ToString("X16");
-                            break;
-                        case 4:
-                            cpukey = FUSELines[4].ToString("X16") + FUSELines[6].ToString("X16");
-                            break;
-                        default:
-                            throw new X360UtilsException(X360UtilsException.X360UtilsErrors.NoValidKeyFound);
-                    }
-                    try {
-                        CpukeyUtils.VerifyCpuKey(cpukey);
-                        return cpukey;
-                    }
-                    catch(X360UtilsException ex) {
-                        if(ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyECD || ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyHamming)
-                            throw; // Dafuq?
-                    }
-                    i++;
-                }
+                if(CheckKey(3, 4, 5, 6))
+                    return (FUSELines[3] | FUSELines[4]).ToString("X16") + (FUSELines[5] | FUSELines[6]).ToString("X16");
+                if(CheckKey(3, 5))
+                    return FUSELines[3].ToString("X16") + FUSELines[5].ToString("X16");
+                if(CheckKey(4, 5))
+                    return FUSELines[4].ToString("X16") + FUSELines[5].ToString("X16");
+                if(CheckKey(3, 6))
+                    return FUSELines[3].ToString("X16") + FUSELines[6].ToString("X16");
+                if(CheckKey(4, 6))
+                    return FUSELines[4].ToString("X16") + FUSELines[6].ToString("X16");
+                throw new X360UtilsException(X360UtilsException.X360UtilsErrors.NoValidKeyFound);
             }
         }
 
         public int CBLDV {
             get {
-                var tmp = FUSELines[2].ToString("X16");
-                return tmp.LastIndexOf("F", StringComparison.Ordinal);
+                var tmp = FUSELines[2].ToString("X16").LastIndexOf("F", StringComparison.Ordinal);
+                return tmp > 0 ? tmp : 0;
             }
         }
 
         public int CFLDV {
             get {
-                var ret = 0;
                 UInt64 l1 = FUSELines[7], l2 = FUSELines[8];
                 if(l1 == 0 && l2 == 0)
                     return 0; // LDV == 0! no need to check ;)
-                for(var i = 0; i < 16; i++) {
-                    if((l1 & 0xF) == 0xF)
+                var ret = 0;
+                for (var i = 0; i < sizeof(UInt64) * 2; i++)
+                {
+                    if ((l1 & 0xF) == 0xF)
                         ret++; // Increment LDV
-                    l1 = l1 >> 8; // Move to the next 8 bits
-                    if((l2 & 0xF) == 0xF)
+                    l1 = l1 >> 4; // Move to the next 4 bits
+                    if ((l2 & 0xF) == 0xF)
                         ret++; // Increment LDV
-                    l2 = l2 >> 8; // Move to the next 8 bits
+                    l2 = l2 >> 4; // Move to the next 4 bits
                 }
                 return ret;
             }
         }
 
-        public bool Retail {
+        public bool FatRetail {
             get { return FUSELines[1] == 0x0F0F0F0F0F0F0FF0; }
         }
 
@@ -95,20 +75,30 @@
             get { return FUSELines[1] == 0x0F0F0F0F0F0F0F0F; }
         }
 
+        public bool Testkit
+        {
+            get { return FUSELines[1] == 0x0F0F0F0F0F0FF00F; }
+        }
+
+        public bool SlimRetail
+        {
+            get { return FUSELines[1] == 0x0F0F0F0F0F0FF0F0; }
+        }
+
         public bool Unlocked {
-            get { return (FUSELines[0] >> 62) == 0xC; }
+            get { return (FUSELines[0] >> 62) == 0x3; }
         }
 
         public bool UsesEeprom {
-            get { return ((FUSELines[0] >> 60) & 0xC) == 0xC; }
+            get { return ((FUSELines[0] >> 60) & 0x3) == 0x3; }
         }
 
         public bool Secure {
-            get { return ((FUSELines[0] >> 58) & 0xC) == 0xC; }
+            get { return ((FUSELines[0] >> 58) & 0x3) == 0x3; }
         }
 
         public bool Invalid {
-            get { return ((FUSELines[0] >> 56) & 0xC) == 0xC; }
+            get { return ((FUSELines[0] >> 56) & 0x3) == 0x3; }
         }
 
         public bool ReservedOk {
@@ -129,6 +119,32 @@
 
         public UInt64 EepromHash2 {
             get { return FUSELines[11]; }
+        }
+
+        private bool CheckKey(int index0, int index1, int index2 = -1, int index3 = -1) {
+            if(index0 >= 0 && index1 >= 0 && index2 >= 0 && index3 >= 0) {
+                try {
+                    CpukeyUtils.VerifyCpuKey(FUSELines[index0] | FUSELines[index1], FUSELines[index2] | FUSELines[index3]);
+                    return true;
+                }
+                catch(X360UtilsException ex) {
+                    if(ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyECD && ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyHamming)
+                        throw; // Dafuq?
+                }
+            }
+            else if(index0 >= 0 && index1 >= 0) {
+                try {
+                    CpukeyUtils.VerifyCpuKey(FUSELines[index0], FUSELines[index1]);
+                    return true;
+                }
+                catch(X360UtilsException ex) {
+                    if(ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyECD && ex.ErrorCode != X360UtilsException.X360UtilsErrors.InvalidKeyHamming)
+                        throw; // Dafuq?
+                }
+            }
+            else
+                throw new ArgumentOutOfRangeException();
+            return false;
         }
     }
 }
